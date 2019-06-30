09c45ad446e0b54c0c48c7ef0927403a8a23177f
Vickymehra83 <52375544+Vickymehra83@users.noreply.github.com>
2019-06-30 21:53:21 +0530

    Add files via upload

---
+++ Questions.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+
+namespace CDSPractical {
+    public class Questions {
+        /// <summary>
+        /// Given an enumerable of strings, attempt to parse each string and if 
+        /// it is an integer, add it to the returned enumerable.
+        /// 
+        /// For example:
+        /// 
+        /// ExtractNumbers(new List<string> { "123", "hello", "234" });
+        /// 
+        /// ; would return:
+        /// 
+        /// {
+        ///   123,
+        ///   234
+        /// }
+        /// </summary>
+        /// <param name="source">An enumerable containing words</param>
+        /// <returns></returns>
+        public IEnumerable<int> ExtractNumbers(IEnumerable<string> source)
+        {
+            List<int> ExtractNumbers = new List<int>();
+            bool chknumber = false;
+            //check if the source has no values
+            if (source != null)
+            {
+                foreach (var item in source)
+                {
+                    int number;
+                    //Parse number to check if it is integer
+                    chknumber = int.TryParse(item, out number);
+                    if(chknumber == true)
+                    {
+                        ExtractNumbers.Add(number);
+                    }
+                    chknumber = false;
+                }
+                return ExtractNumbers;
+            }
+            throw new NotImplementedException();
+        
+        }
+
+        /// <summary>
+        /// Given two enumerables of strings, find the longest common word.
+        /// 
+        /// For example:
+        /// 
+        /// LongestCommonWord(
+        ///     new List<string> {
+        ///         "love",
+        ///         "wandering",
+        ///         "goofy",
+        ///         "sweet",
+        ///         "mean",
+        ///         "show",
+        ///         "fade",
+        ///         "scissors",
+        ///         "shoes",
+        ///         "gainful",
+        ///         "wind",
+        ///         "warn"
+        ///     },
+        ///     new List<string> {
+        ///         "wacky",
+        ///         "fabulous",
+        ///         "arm",
+        ///         "rabbit",
+        ///         "force",
+        ///         "wandering",
+        ///         "scissors",
+        ///         "fair",
+        ///         "homely",
+        ///         "wiggly",
+        ///         "thankful",
+        ///         "ear"
+        ///     }
+        /// );
+        /// 
+        /// ; would return "wandering" as the longest common word.
+        /// </summary>
+        /// <param name="first">First list of words</param>
+        /// <param name="second">Second list of words</param>
+        /// <returns></returns>
+        public string LongestCommonWord(IEnumerable<string> first, IEnumerable<string> second)
+        {
+
+            string lcommonword = string.Empty;
+            if (first != null && second != null)
+            {
+                if (first.Count() > 0 && second.Count() > 0)
+                {
+                    try
+                    {
+                        lcommonword = first.Join(second, str1 => str1, str2 => str2, (str1, str2) => str1)
+                                                       .OrderByDescending(x => x.Length)
+                                                        .FirstOrDefault();
+                        if(!string.IsNullOrEmpty(lcommonword))
+                        {
+                            return lcommonword;
+                        }
+                        //else
+                        //{
+                        //    return 0;
+                        //}
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error reading from {0}. Message = {1}",ex.Message);
+                    }
+                }
+            }
+                throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that converts kilometers to miles, given that there are
+        /// 1.6 kilometers per mile.
+        /// 
+        /// For example:
+        /// 
+        /// DistanceInMiles(16.00);
+        /// 
+        /// ; would return 10.00;
+        /// </summary>
+        /// <param name="km">distance in kilometers</param>
+        /// <returns></returns>
+        public double DistanceInMiles(double km)
+        {
+            double distanceinmiles = 0;
+            const double milestokilometer = 1.6;
+            if (km > 0)
+            {
+                distanceinmiles = km / milestokilometer;
+                return distanceinmiles;
+            }
+            else
+            {
+                return 0;
+            }
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that converts miles to kilometers, give that there are
+        /// 1.6 kilometers per mile.
+        /// 
+        /// For example:
+        /// 
+        /// DistanceInKm(10.00);
+        /// 
+        /// ; would return 16.00;
+        /// </summary>
+        /// <param name="miles">distance in miles</param>
+        /// <returns></returns>
+        public double DistanceInKm(double miles)
+        {
+            double distanceinkm = 0;
+            const double kmtomiles = 1.6;
+            if (miles > 0)
+            {
+                distanceinkm = miles * kmtomiles;
+                return distanceinkm;
+            }
+            else
+            {
+                return 0;
+            }
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that returns true if the word is a palindrome, false if
+        /// it is not.
+        /// 
+        /// For example:
+        /// 
+        /// IsPalindrome("bolton");
+        /// 
+        /// ; would return false, and:
+        /// 
+        /// IsPalindrome("Anna");
+        /// 
+        /// ; would return true.
+        /// 
+        /// Also complete the related test case for this method.
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns></returns>
+        public bool IsPalindrome(string word)
+        {
+           
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                {
+                    return false;
+                }
+                return true;
+            }
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that takes an enumerable list of objects and shuffles
+        /// them into a different order.
+        /// 
+        /// For example:
+        /// 
+        /// Shuffle(new List<string>{ "one", "two" });
+        /// 
+        /// ; would return:
+        /// 
+        /// {
+        ///   "two",
+        ///   "one"
+        /// }
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public IEnumerable<object> Shuffle(IEnumerable<object> source)
+        {
+            return source.OrderBy(a => Guid.NewGuid()).ToList();
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that sorts an array of integers into ascending
+        /// order - do not use any built in sorting mechanisms or frameworks.
+        /// 
+        /// Complete the test for this method.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public int[] Sort(int[] source)
+        {
+            throw new NotImplementedException();
+        }    
+
+        /// <summary>
+        /// Each new term in the Fibonacci sequence is generated by adding the 
+        /// previous two terms. By starting with 1 and 2, the first 10 terms will be:
+        ///
+        /// 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+        ///
+        /// By considering the terms in the Fibonacci sequence whose values do 
+        /// not exceed four million, find the sum of the even-valued terms.
+        /// </summary>
+        /// <returns></returns>
+        public int FibonacciSum()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate a list of integers from 1 to 100.
+        /// 
+        /// This method is currently broken, fix it so that the tests pass.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<int> GenerateList() {
+            var ret = new List<int>();
+            var numThreads = 2;
+
+            Thread[] threads = new Thread[numThreads];
+            for (var i = 0; i < numThreads; i++) {
+                threads[i] = new Thread(() => {
+                    var complete = false;
+                    while (!complete) {                        
+                        var next = ret.Count + 1;
+                        Thread.Sleep(new Random().Next(1, 10));
+                        if (next <= 100) {
+                            ret.Add(next);
+                        }
+
+                        if (ret.Count >= 100) {
+                            complete = true;
+                        }
+                    }                    
+                });
+                threads[i].Start();
+            }
+
+            for (var i = 0; i < numThreads; i++) {
+                threads[i].Join();
+            }
+
+            return ret;
+        }
+    }
+}
